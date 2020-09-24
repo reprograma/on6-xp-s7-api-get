9916cea5692a5e9bec2cc81bd7df2cde4169e13c
botelholarissa <conta.larissa@gmail.com>
2020-09-24 20:20:52 -0300

    Resolvido getByYear

--- para-o-lar/servidor-larissa/src/controllers/filmesController.js
+++ para-o-lar/servidor-larissa/src/controllers/filmesController.js
@@ -9,13 +9,13 @@ const getAll = (request, response) => {
 
 const getByYear = (request, response) => {
     const year = request.params.year
-    const yearExists = filmes.filter(filme => filmes.Year == year)
-    console.log(yearExists)
+    const filmeAno = filmes.find(filme => filme.Year == year)
+    console.log(filmeAno)
     
-    if(!yearExists){//não consegui identificar como vou representar que ano não existe
+    if(!filmeAno){//não consegui identificar como vou representar que ano não existe
         response.status(404).send({message: "Esse ano não existe."})//Não está retornando a mensagem quando o ano é inválido
     } else {
-        response.status(200).send(filmes.find(filme => filme.Year == year))
+        response.status(200).send(filmeAno)
     }
 }
 
