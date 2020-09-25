f1d7425f1a4a06dd6c4128d5fd20b9e4a9dd7475
TainaraMoreno <tainaramoreno89@gmail.com>
2020-09-25 13:01:09 -0300

    Atividade Pronta para entrega

--- para-o-lar/server.js
+++ para-o-lar/server.js
@@ -1,2 +1,8 @@
-const express = require('express')
-const app = express()
+
+const app = require("./src/app")
+
+const PORT = 8080
+
+app.listen(PORT, function(){
+    console.log(" O nosso app está rodando na porta " + PORT)
+} )
