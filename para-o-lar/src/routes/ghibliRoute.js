b64e01d744d4b685651dc41e790e2b8fc96d5184
luzialeticia <leticialuzia@tutamail.com>
2020-09-25 03:47:26 -0300

    Criando a rota para chamar filme por id

--- para-o-lar/src/routes/ghibliRoute.js
+++ para-o-lar/src/routes/ghibliRoute.js
@@ -3,5 +3,6 @@ const router = express.Router()
 const controller = require("../controller/ghibliController")
 
 router.get("/", controller.getAll)
+router.get("/:id", controller.getById)
 
 module.exports =  router
