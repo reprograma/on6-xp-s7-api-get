b23e985065a022a2aca5fac7ac90755860c03556
luzialeticia <leticialuzia@tutamail.com>
2020-09-25 03:20:10 -0300

    Criando a rota para o model ghibli

---
+++ para-o-lar/src/routes/ghibliRoute.js
@@ -0,0 +1,7 @@
+const express = require("express")
+const router = express.Router()
+const controller = require("../controller/ghibliController")
+
+router.get("/", controller.getAll)
+
+module.exports =  router
