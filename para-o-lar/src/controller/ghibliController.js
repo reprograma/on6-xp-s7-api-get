38a84a2624d92fbbb56c252498224bd75a5f0fc3
luzialeticia <leticialuzia@tutamail.com>
2020-09-25 03:46:39 -0300

    Criando a função para selecionar o filme por id

--- para-o-lar/src/controller/ghibliController.js
+++ para-o-lar/src/controller/ghibliController.js
@@ -1,10 +1,24 @@
+const { request, response } = require("../app")
 const ghibli = require("../models/ghibli.json")
 
+//Para retornar todos os filmes
 const getAll = (request, response) =>{
     console.log(request.url)
     response.status(200).send(ghibli)
 }
 
+//Para retornar apenas 1 filmes, selecionado pelo id
+const getById = (request, response) => {
+    const id = request.params.id
+    const idRequested = ghibli.find(item => item.id == id)
+
+    if(!idRequested) {
+        return response.status(400).send({ message: 'Filme não encontrado.'})
+    }
+    return response.status(200).send(idRequested)
+}
+
 module.exports = {
-    getAll
+    getAll,
+    getById
 }
