98f3ca904d34bb081bb4a7df6cb2eb7ce86722b3
camila-marquesbr <camiladev.br@gmail.com>
2020-09-25 18:42:49 -0300

    get all Routers

--- para-o-lar/app.js
+++ para-o-lar/app.js
@@ -4,7 +4,15 @@ const app = express()
 
 //rotas
 const filmes = require("./src/routers/filmesRoute")
+app.use("/filmes", filmes )
 
-app.use("/", filmes )
+const ghibli = require("./src/routers/ghibliRouter")
+app.use("/ghibli", ghibli)
+
+const estados= require("./src/routers/estadosRouter")
+app.use("/estados", estados)
+
+const colors = require("./src/routers/colorsRouter")
+app.use("/colors", colors)
 
 module.exports = app
