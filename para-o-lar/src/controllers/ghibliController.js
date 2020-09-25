e31777fa242f0c85036683491aa4551dea36ce5a
camila-marquesbr <camiladev.br@gmail.com>
2020-09-25 16:37:41 -0300

    ghibli controller

---
+++ para-o-lar/src/controllers/ghibliController.js
@@ -0,0 +1,9 @@
+const ghibli = require("../models/ghibli.json")
+
+const getAll =  (request, response) => {
+    console.log(request.url)
+    response.status(200).send(ghibli)
+}
+module.exports = {
+    getAll 
+}
