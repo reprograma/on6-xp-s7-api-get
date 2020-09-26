30699b1b16594890968fd7d0cdf6c05810e93cfd
luzialeticia <leticialuzia@tutamail.com>
2020-09-26 02:07:44 -0300

    Criando a função pra pesquisar um estado pela sigla e retornar todas as cidades

--- para-o-lar/src/controller/estadosController.js
+++ para-o-lar/src/controller/estadosController.js
@@ -2,10 +2,22 @@ const { request, response } = require("../app")
 const estados = require("../models/estados-cidades.json")
 
 //Retorna todos os estados
-const getAll = (request, response) =>{
+const getAll = (request, response) => {
     response.status(200).send(estados)
 }
 
+//Retorna um estado e todas as suas cidades
+const getAllCities = (request, response) => {
+    const estate = request.params.estate
+    const searchEstate = estados.find(item => estate.toUpperCase() == item.sigla)
+
+    if(!searchEstate) {
+        return response.status(400).send('Estado não encontrado.')
+    }
+    return response.status(200).send(searchEstate)
+}
+
 module.exports = {
-    getAll
+    getAll,
+    getAllCities
 }
