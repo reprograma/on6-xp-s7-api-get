4e235f1f7cd8d4381b63f4390b13cc0aeab0b7e2
mrnpinheiro <mrndsp@gmail.com>
2020-09-25 21:54:31 -0300

    Add config + APIs.

---
+++ para-o-lar/src/routes/estadosRoute.js
@@ -0,0 +1,7 @@
+const express = require("express")
+const router = express.Router()
+const controller = require("../controller/estadosController")
+
+router.get("/", controller.getAll)
+
+module.exports = router
