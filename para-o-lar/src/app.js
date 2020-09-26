4ab751899e77967eb421e5a0cf0323fec4626f87
luzialeticia <leticialuzia@tutamail.com>
2020-09-26 01:44:09 -0300

    Criando a rota para o estados no app

--- para-o-lar/src/app.js
+++ para-o-lar/src/app.js
@@ -8,4 +8,7 @@ app.use("/ghibli", ghibli)
 const filmes = require("./routes/filmesRoute")
 app.use("/filmes", filmes)
 
+const estados = require("./routes/estadosRoute")
+app.use("/estados", estados)
+
 module.exports = app
