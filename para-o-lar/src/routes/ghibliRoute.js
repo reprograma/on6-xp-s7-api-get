c20f6360c23eb1e2a4f4803cb184a19e6351d8ff
luzialeticia <leticialuzia@tutamail.com>
2020-09-25 21:48:23 -0300

    Comentando rota para pesquisa po título

--- para-o-lar/src/routes/ghibliRoute.js
+++ para-o-lar/src/routes/ghibliRoute.js
@@ -5,5 +5,6 @@ const controller = require("../controller/ghibliController")
 router.get("/", controller.getAll)
 router.get("/:id", controller.getById)
 router.get("/:filmeGenre/buscar", controller.getByGenre)
+//router.get("/:filmeTitle/buscar", controller.getByTitle)
 
 module.exports =  router
