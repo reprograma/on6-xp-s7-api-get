bf7f91b4d0d18ab5c18bd3f9b873f0c480bfe2e4
luzialeticia <leticialuzia@tutamail.com>
2020-09-26 03:29:34 -0300

    Chamando a variável database

--- para-o-lar/src/controller/estadosController.js
+++ para-o-lar/src/controller/estadosController.js
@@ -1,15 +1,15 @@
 const { request, response } = require("../app")
-const estados = require("../models/estados-cidades.json")
+const dataBase = require("../models/estados-cidades-copy.json")
 
 //Retorna todos os estados
 const getAll = (request, response) => {
-    response.status(200).send(estados)
+    response.status(200).send(dataBase.estados)
 }
 
 //Retorna um estado pela sigla e todas as suas cidades
 const getState = (request, response) => {
     const state = request.params.state
-    const searchState = estados.find(item => state.toUpperCase() == item.sigla)
+    const searchState = dataBase.estados.find(item => state.toUpperCase() == item.sigla)
 
     if(!searchState) {
         return response.status(400).send('Estado não encontrado.')
@@ -20,7 +20,7 @@ const getState = (request, response) => {
 //Retorna os estados que contenham determinada palavra inclusa em seu nome
 const getStateByName = (request, response) => {
     const state = request.params.state
-    const searchState = estados.filter(item => item.nome.toLowerCase().includes(state))
+    const searchState = dataBase.estados.filter(item => item.nome.toLowerCase().includes(state))
 
     if(searchState == false) {
         return response.status(400).send('Estado não encontrado.')
