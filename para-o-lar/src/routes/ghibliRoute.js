5a8e51707b0384045fbb1d206ae07315555055f9
luzialeticia <leticialuzia@tutamail.com>
2020-09-25 23:45:36 -0300

    Editando a rota pra procurar por título

--- para-o-lar/src/routes/ghibliRoute.js
+++ para-o-lar/src/routes/ghibliRoute.js
@@ -5,6 +5,6 @@ const controller = require("../controller/ghibliController")
 router.get("/", controller.getAll)
 router.get("/:id", controller.getById)
 router.get("/:filmeGenre/buscar", controller.getByGenre)
-//router.get("/:filmeTitle/buscar", controller.getByTitle)
+router.get("/:filmeTitle/search", controller.getByTitle)
 
-module.exports =  router
+module.exports = router
