7fe6394dc96f15407f5d63029edb220b36aad699
luzialeticia <leticialuzia@tutamail.com>
2020-09-26 02:08:05 -0300

    Criando a rota pra retornar o estado e suas cidades

--- para-o-lar/src/routes/estadosRoute.js
+++ para-o-lar/src/routes/estadosRoute.js
@@ -3,5 +3,6 @@ const router = express.Router()
 const controller = require("../controller/estadosController")
 
 router.get("/", controller.getAll)
+router.get("/:estate", controller.getAllCities)
 
 module.exports = router
